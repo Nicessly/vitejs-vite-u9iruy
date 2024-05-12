708a9935dab97404744a69f9eafe01b5139c4ab8
Nicessly <nicessly3@gmail.com>
2024-05-12 17:14:22 -0300

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from "./Nav/nav";
 import Menu from './Menu/menu';
 import Post from './Post/post';
 import FeedList from './Card/CardList';
 import BuscarPersonas from './Buscar/buscar';
 import CalendarioB from './Calendario/calendarioB';
+import BuscarPersonasPage from './BuscarPersonasPage/BuscarPersonasPage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <Nav/> 
-      <div className="container">
-        <main>
-          <Menu/>
-          <div className="post-and-feed">
-            <Post/>
-            <FeedList/>
-          </div>
-          <CalendarioB/>
-        </main>
+    <Router>
+      <div>
+        <Nav />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact>
+              <Menu />
+              <main>
+                <div className="post-and-feed">
+                  <Post />
+                  <FeedList />
+                </div>
+                <CalendarioB />
+              </main>
+            </Route>
+            <Route path="/buscar-personas">
+              <BuscarPersonasPage />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
-  )
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/BuscarPersonasPage/BuscarPersonasPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from '../Nav/nav';
+import Menu from '../Menu/menu';
+import BuscarPersonas from '../Buscar/buscar';
+
+
+function BuscarPersonasPage() {
+  return (
+    <div>
+      <Nav />
+      <Menu />
+      <BuscarPersonas/>
+    </div>
+  );
+}
+
+export default BuscarPersonasPage;
