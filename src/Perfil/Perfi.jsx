ed9331229ded02572dcffbad6900e2692b1e42e2
Nicessly <nicessly3@gmail.com>
2024-05-13 23:28:44 -0300

    peril listo bien

--- src/Perfil/Perfi.jsx
+++ src/Perfil/Perfi.jsx
@@ -1,41 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./perfil.css"
-import Perfill from "./perfil.jpg"
-import FeedList from '../Card/CardList';
+import PerfilImg from "./perfil.jpg"
+import { UilImageEdit } from '@iconscout/react-unicons'
 
 function Perfil() {
-    return (
-      <main className='mover'>
-       <div className="middle">
+  // Establecer imágenes por defecto para portada y foto de perfil
+  const defaultCoverPhoto = "https://img.freepik.com/foto-gratis/fondo-gris-liso-alta-calidad_53876-124606.jpg";
+  const defaultProfilePhoto = "https://w7.pngwing.com/pngs/1000/665/png-transparent-computer-icons-profile-s-free-angle-sphere-profile-cliparts-free.png";
+
+  // Utilizar useState con valores iniciales
+  const [coverPhoto, setCoverPhoto] = useState(defaultCoverPhoto);
+  const [profilePhoto, setProfilePhoto] = useState(defaultProfilePhoto);
+
+  const handleCoverPhotoChange = (event) => {
+    const newCoverPhoto = URL.createObjectURL(event.target.files[0]);
+    setCoverPhoto(newCoverPhoto);
+  }
+
+  const handleProfilePhotoChange = (event) => {
+    const newProfilePhoto = URL.createObjectURL(event.target.files[0]);
+    setProfilePhoto(newProfilePhoto);
+  }
+
+  return (
+    <main className='mover'>
+      <div className="middle">
         <div className="perfil-facebook">
           {/* Imagen de portada */}
           <div className="cover-photo">
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Nelumno_nucifera_open_flower_-_botanic_garden_adelaide.jpg/1200px-Nelumno_nucifera_open_flower_-_botanic_garden_adelaide.jpg" // Coloca la URL de la imagen de portada aquí
+              src={coverPhoto}
               alt="Portada"
               className="portada"
             />
-            {/* Foto de perfil */}
+            <label htmlFor="coverPhotoInput">
+              <UilImageEdit className="porta" />
+              <input
+                id="coverPhotoInput"
+                type="file"
+                accept="image/*"
+                onChange={handleCoverPhotoChange}
+                style={{ display: 'none' }}
+              />
+            </label>
+          </div>
+          {/* Foto de perfil */}
+          <div className="profile-photo">
             <img
-              src={Perfill} // Coloca la URL de la imagen de perfil aquí
+              src={profilePhoto}
               alt="Foto de perfil"
               className="foto-perfil"
             />
+            <label htmlFor="profilePhotoInput">
+              <UilImageEdit className="perfi" />
+              <input
+                id="profilePhotoInput"
+                type="file"
+                accept="image/*"
+                onChange={handleProfilePhotoChange}
+                style={{ display: 'none' }}
+              />
+            </label>
           </div>
           {/* Información del perfil */}
           <div className="infot">
             <h2>Lana Rose</h2>
             {/* Seguidores y posts */}
             <div className="datos">
-              <span>Seguidores: 100</span>
-              <span>|</span>
               <span>Post subidos: 2</span>
             </div>
           </div>
         </div>
-        </div>
-        </main>
-        
+      </div>
+    </main>
   );
 }
 
