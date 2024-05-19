88bcabdba33cb0ca37b32f3135d3ecf3fbcf9d79
Nicessly <nicessly3@gmail.com>
2024-05-18 23:01:41 -0300

    el log out bienn

--- src/Nav/nav.jsx
+++ src/Nav/nav.jsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UilSignout } from '@iconscout/react-unicons'
 import './nav.css'
+import log from "./logi.png"
 
 function Nav() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleLogout = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleAcceptLogout = () => {
+    // Aquí puedes agregar la lógica para cerrar la sesión
+    console.log('Cerrando sesión...');
+    handleCloseModal();
+  };
+
+  
+
   return (
     <nav>
       <div className="container">
         <h2 className="logo">ZYPHY</h2>
         <div className="create">
-          <div className="profil">
-            <UilSignout />
+          <div className="profil" >
+          <UilSignout onClick={handleLogout} />
           </div>
         </div>
       </div>
+      {showModal && (
+        <div className="modall show">
+          <div className="modall-content">
+          <img src={log} className='log'/>
+          <h4 className='h4'>ComeBack Some!</h4>
+            <p>Are you sure yoy want<br/> to logout?</p>
+            <button onClick={handleCloseModal} className='negar'>Cancel</button>
+            <button onClick={handleAcceptLogout} className='aceptar'>Yes, logout</button>
+          </div>
+        </div>
+      )}
     </nav>
-
   );
 }
 
